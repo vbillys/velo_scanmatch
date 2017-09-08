1d212c29020a7590a6eadbf4f195d56696ff4f49
Zhong Yi <zhongy@i2r.a-star.edu.sg>
2017-09-08 16:55:18 +0800

    still buggy, angle heading changes disabled

--- src/snapshotter.cpp
+++ src/snapshotter.cpp
@@ -7,6 +7,15 @@
 // Messages
 #include "sensor_msgs/PointCloud2.h"
 
+// tf
+#include "tf/transform_broadcaster.h"
+#include "tf/transform_listener.h"
+#include "tf_conversions/tf_eigen.h"
+
+// helper functions
+#include "helper/yaw_angle_calc.hpp"
+
+
 
 /***
  * This a simple test app that requests a point cloud from the
@@ -19,10 +28,19 @@ namespace laser_assembler
 class PeriodicSnapshotter
 {
 
+  // callback for caching latest tf from pc2
+  void pc2Callback(const sensor_msgs::PointCloud2ConstPtr& msg)
+  {
+    latest_pc2_time=msg->header.stamp;
+    waiting_for_pc2 = false;
+  }
 public:
 
   PeriodicSnapshotter()
   {
+    // indicate that we are waiting for first pc2 msg
+    waiting_for_pc2 = true;
+
     // Create a publisher for the clouds that we assemble
     pub_ = n_.advertise<sensor_msgs::PointCloud2> ("assembled_cloud2", 1);
 
@@ -30,48 +48,158 @@ public:
     client_ = n_.serviceClient<AssembleScans2>("assemble_scans2");
 
     // Start the timer that will trigger the processing loop (timerCallback)
-    timer_ = n_.createTimer(ros::Duration(5,0), &PeriodicSnapshotter::timerCallback, this);
+    //timer_ = n_.createTimer(ros::Duration(5,0), &PeriodicSnapshotter::timerCallback, this);
+    timer_ = n_.createTimer(ros::Duration(0.5), &PeriodicSnapshotter::timerCallback, this);
 
     // Need to track if we've called the timerCallback at least once
     first_time_ = true;
+
+    // setup watch on the cloud itself to track the tf
+    sub_ = n_.subscribe("/velodyne_cloud_registered",1,&laser_assembler::PeriodicSnapshotter::pc2Callback, this);
   }
+
+  void getCurrentCameraPose(const ros::Time & t, tf::Stamped<tf::Pose> & pose);
 
   void timerCallback(const ros::TimerEvent& e)
   {
+    // dummy variable (not used in SE2)
+    double pitch, roll;
+    // helping to get axis angle representation
+    Eigen::Matrix3d emat;
+
+    // check if first cloud has arrived
+    if (waiting_for_pc2) return;
 
     // We don't want to build a cloud the first callback, since we we
     //   don't have a start and end time yet
     if (first_time_)
     {
       first_time_ = false;
+      // use this first one as first ref to estimate motion
+      prev_tref = latest_pc2_time; //e.current_real;
+      // get current position of camera
+      // as last position
+      // TODO: extend to SE3
+      //getCurrentCameraPose(e.current_real, last_tf_pose);
+      getCurrentCameraPose(latest_pc2_time, last_tf_pose);
+      x_prev = last_tf_pose.getOrigin().x();
+      y_prev = last_tf_pose.getOrigin().y();
+      //last_tf_pose.getBasis().getEulerYPR(yaw_prev, pitch, roll);
+      //last_tf_pose.getBasis().getEulerYPR(pitch, yaw_prev, roll);
+      last_tf_pose.getBasis().getRPY(pitch, yaw_prev, roll);
+      tf::matrixTFToEigen(last_tf_pose.getBasis(), emat);
+      Eigen::AngleAxisd angleaxis(emat);
+      yaw_prev = angleaxis.angle();
       return;
     }
 
-    // Populate our service request based on our timer callback times
-    AssembleScans2 srv;
-    srv.request.begin = e.last_real;
-    srv.request.end   = e.current_real;
-
-    // Make the service call
-    if (client_.call(srv))
+    // check if camera has moved certain distance or heading
+    bool has_moved = false;
+    // get current one and compare with previous
+    //getCurrentCameraPose(e.current_real, current_tf_pose);
+    getCurrentCameraPose(latest_pc2_time, current_tf_pose);
+    // get details can collect distance
+    // TODO: extend to SE3
+    double x_now = current_tf_pose.getOrigin().x();
+    double y_now = current_tf_pose.getOrigin().z();
+    double delta_x = x_now - x_prev;
+    double delta_y = y_now - y_prev;
+    double dist = sqrt ( delta_x*delta_x + delta_y*delta_y );
+    // WARNING: HARDCODED Thresh
+    if ( 1.5 <= dist ) 
     {
-      //ROS_INFO("Published Cloud with %u points", (uint32_t)(srv.response.cloud.points.size())) ;
-      ROS_INFO("Published Cloud with points") ;
-      pub_.publish(srv.response.cloud);
+      ROS_INFO("Triggering because translation of: %.2f", dist);
+      has_moved = true;
     }
-    else
+    else ROS_INFO("trans is : x:%.2f y:%.2f", delta_x, delta_y);
+
+
+    // get details can collect heading change
+    // TODO: extend to SE3
+    double yaw_now;
+    //if (! has_moved)
+    //{
+      //current_tf_pose.getBasis().getEulerYPR(yaw_now, pitch , roll);
+      //current_tf_pose.getBasis().getEulerYPR(pitch, yaw_now , roll);
+      current_tf_pose.getBasis().getRPY(pitch, yaw_now , roll);
+      tf::matrixTFToEigen(current_tf_pose.getBasis(), emat);
+      Eigen::AngleAxisd angleaxis(emat);
+      yaw_now = angleaxis.angle();
+      double delta_yaw = ANGLEDIFF(yaw_now , yaw_prev);
+      // WARNING: HARDCODED Thresh
+      if (0.25 <= delta_yaw) 
+      {
+	ROS_INFO("Triggering because rotation of: %.2f %.2f", yaw_now, delta_yaw);
+	//has_moved = true;
+      }
+      else ROS_INFO("rot is : %.2f %.2f %.2f", yaw_now, yaw_prev, delta_yaw);
+    //}
+
+    if (has_moved)
     {
-      ROS_ERROR("Error making service call\n") ;
+      // Populate our service request based on our timer callback times
+      AssembleScans2 srv;
+      //srv.request.begin = e.last_real;
+      srv.request.begin = prev_tref;
+      srv.request.end   = e.current_real;
+      // store previous time and pose for next iteration
+      prev_tref = e.current_real;
+      last_tf_pose = current_tf_pose;
+      // TODO: extend to SE3
+      x_prev = x_now;
+      y_prev = y_now;
+      yaw_prev = yaw_now;
+
+      // Make the service call
+      if (client_.call(srv))
+      {
+	//ROS_INFO("Published Cloud with %u points", (uint32_t)(srv.response.cloud.points.size())) ;
+	ROS_INFO("Published Cloud with points") ;
+	pub_.publish(srv.response.cloud);
+      }
+      else
+      {
+	ROS_ERROR("Error making service call\n") ;
+      }
     }
   }
 
 private:
   ros::NodeHandle n_;
   ros::Publisher pub_;
+  ros::Subscriber sub_;
   ros::ServiceClient client_;
   ros::Timer timer_;
   bool first_time_;
+  ros::Time prev_tref;
+  ros::Time latest_pc2_time;
+  
+  tf::TransformListener tf_listen_;
+  tf::TransformBroadcaster tf_caster_;
+  tf::Stamped<tf::Pose> current_tf_pose;
+  tf::Stamped<tf::Pose> last_tf_pose;
+
+  double x_prev;
+  double y_prev;
+  double yaw_prev;
+  bool waiting_for_pc2;
 } ;
+
+// Give time to transform look the camera pose
+// Carefull the camera frame id is HARDCODED
+void PeriodicSnapshotter::getCurrentCameraPose(const ros::Time & t, tf::Stamped<tf::Pose> & pose)
+{
+  tf::Stamped<tf::Pose> ident (tf::Transform(tf::createIdentityQuaternion(), tf::Vector3(0,0,0)), t, "camera");
+  try
+  {
+    this->tf_listen_.transformPose("camera_init", ident, pose);
+  }
+  catch(tf::TransformException e)
+  {
+    ROS_WARN("Failed to compute odom pose, skipping scan (%s)", e.what());
+  }
+}
+
 
 }
 
