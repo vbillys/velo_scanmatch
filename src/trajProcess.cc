a2366c1db3fce7d89aed8ee3a38e8e79d51ae698
Billy <billy_saputra@i2r.a-star.edu.sg>
2018-02-23 09:48:12 +0800

    added publishing to rviz, also remove NaN

--- src/trajProcess.cc
+++ src/trajProcess.cc
@@ -35,17 +35,19 @@
 
 #include "sensor_msgs/PointCloud2.h"
 
+#include "transform.h"
 #include "transform_interpolation_buffer.h"
 
 #include "tf/transform_datatypes.h"
 
 #include "point_types.h"
+#include <pcl/filters/voxel_grid.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <pcl_conversions/pcl_conversions.h>
 typedef velodyne_pointcloud::PointXYZIR VPoint;
-//typedef pcl::PointCloud<VPoint> VPointCloud;
-typedef pcl::PointCloud<pcl::PointXYZ> VPointCloud;
+typedef pcl::PointCloud<VPoint> VPointCloud;
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 DEFINE_string(traj_filename, "",
     "Proto stream file containing the pose graph.");
@@ -62,6 +64,13 @@ int main(int argc, char** argv) {
     << "-traj_filename is missing.";
   CHECK(!FLAGS_bag_filenames.empty()) << "-bag_filenames is missing.";
 
+  ros::init(argc, argv, "trajProcess");
+  ros::NodeHandle nh, pnh("~");
+  bool b_opt_publish_pc;
+  pnh.param("publish_pc", b_opt_publish_pc, false);
+
+  ros::Publisher pc_pub = nh.advertise<sensor_msgs::PointCloud2>("total_pc", 1, true);
+
   ROS_INFO("Using traj file: %s", FLAGS_traj_filename.c_str());
   cartographer::io::ProtoStreamReader reader(FLAGS_traj_filename);
   cartographer::mapping::proto::Trajectory proto_traj;
@@ -70,7 +79,8 @@ int main(int argc, char** argv) {
 
   for (auto i : proto_traj.node())
   {
-    ROS_INFO("%.6f", cartographer_ros::ToRos(cartographer::common::FromUniversal(i.timestamp())).toSec());
+    ROS_INFO("%.10f", cartographer_ros::ToRos(cartographer::common::FromUniversal(i.timestamp())).toSec());
+    //LOG(INFO) << cartographer::transform::ToRigid3(i.pose());
   }
 
   const cartographer::transform::TransformInterpolationBuffer
@@ -94,7 +104,14 @@ int main(int argc, char** argv) {
     // TODO: Put additional spacing condition here...
     if(transform_interpolation_buffer.Has(cartographer_ros::FromRos(pc2_msg->header.stamp)))
     {
-      pcl_pointclouds.push_back( pcl_cloud );
+      PointCloud pcl_cloud_noNaN;
+      std::vector<int> indices;
+      PointCloud t_ppcl_cloud;
+      pcl::copyPointCloud(pcl_cloud, t_ppcl_cloud);
+      pcl::removeNaNFromPointCloud(t_ppcl_cloud, pcl_cloud_noNaN, indices);
+      VPointCloud pcl_vcloud_noNaN;
+      pcl::copyPointCloud(pcl_cloud, indices, pcl_vcloud_noNaN);
+      pcl_pointclouds.push_back( pcl_vcloud_noNaN);
 
       const cartographer::transform::Rigid3d tracking_to_map =
 	transform_interpolation_buffer.Lookup(cartographer_ros::FromRos(pc2_msg->header.stamp));
@@ -103,22 +120,42 @@ int main(int argc, char** argv) {
       //const cartographer::transform::Rigid3f sensor_to_map =
 	//(tracking_to_map * sensor_to_tracking).cast<float>();
       odometry_poses.push_back(tf::Transform( tf::Quaternion(tracking_to_map.rotation().x(),tracking_to_map.rotation().y(),tracking_to_map.rotation().z(),tracking_to_map.rotation().w()), tf::Vector3(tracking_to_map.translation().x(),tracking_to_map.translation().y(),tracking_to_map.translation().z())));
+      LOG(INFO) << tracking_to_map;
     }
 
-    ROS_INFO("Time message: %.6f %s points:%lu", pc2_msg->header.stamp.toSec(),
+    // test ring info
+    //int no_of_points = 0;
+    //for (VPointCloud::iterator p=pcl_cloud.begin(); p!= pcl_cloud.end(); p++)
+    //{
+       //if (p->ring == 7) no_of_points++;
+    //}
+    //ROS_INFO("no_of_points: %d", no_of_points);
+
+    ROS_INFO("Time message: %.10f %s points:%lu", pc2_msg->header.stamp.toSec(),
       transform_interpolation_buffer.Has(cartographer_ros::FromRos(pc2_msg->header.stamp))?"has":"not", pcl_cloud.size());
   }
   bag.close();
 
 
-  int index = 0; VPointCloud total_pointcloud;
+  int index = 0; PointCloud total_pointcloud;
   for (auto curr_pc : pcl_pointclouds)
   {
-     VPointCloud transformed_pointcloud;
-     pcl_ros::transformPointCloud(curr_pc, transformed_pointcloud, tf::Pose(odometry_poses[index]) * tf::Pose(tf::Quaternion(), tf::Vector3()));
+     PointCloud transformed_pointcloud;
+     PointCloud t_pc; pcl::copyPointCloud(curr_pc, t_pc);
+     pcl_ros::transformPointCloud(t_pc, transformed_pointcloud, tf::Pose(odometry_poses[index]) * tf::Pose(tf::Quaternion(), tf::Vector3()));
      total_pointcloud += transformed_pointcloud;
      index++;
   }
 
   ROS_INFO("Clouds size: %lu, odometry_poses size: %lu, total_points: %lu", pcl_pointclouds.size(), odometry_poses.size(), total_pointcloud.size());
+
+  //total_pointcloud = PointCloud(pcl_pointclouds[0]);
+  //pcl::copyPointCloud ( pcl_pointclouds[0], total_pointcloud);
+  total_pointcloud.header.frame_id = "map";
+  total_pointcloud.header.stamp    = ros::Time::now().toNSec();
+
+
+  if (b_opt_publish_pc)
+    pc_pub.publish(total_pointcloud);
+  ros::spin();
 }
