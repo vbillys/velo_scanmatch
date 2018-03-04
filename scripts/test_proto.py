bfaa1beb0f8ac0dac4e4f7c943438cf3e74e60b6
Billy <billy_saputra@i2r.a-star.edu.sg>
2018-03-04 23:48:40 +0800

    added python script to read protobuf, not using gzipped - NOT WORKING YET.

---
+++ scripts/test_proto.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+import trajectory_pb2
+import zlib
+
+# f = open('/home/agoy/Documents/denso_calib_2/denso_calib_2_0.pbstream','r');
+f = open('/home/agoy/Documents/denso_calib_2/test.pbstream','r');
+fs = f.read()
+s = fs[0:8];
+print list(bytearray(s))
+print map(hex,bytearray(s))
+
+# ignore those magic bytes already
+
+# size is size_t 8 bytes
+s = fs[8:16]
+print list(bytearray(s))
+print map(hex,bytearray(s))
+
+# s = fs[16:221616]
+# print len(s)
+# # protobuf_stream = zlib.decompress(s, 15 + 32)
+# # protobuf_stream = zlib.decompress(s, zlib.MAX_WBITS | 16)
+# protobuf_stream = zlib.decompress(fs[16:221616], zlib.MAX_WBITS | 16)
+# protobuf_stream = zlib.decompress(fs[16:221616], zlib.MAX_WBITS | 32)
+
+# print len(protobuf_stream)
+
+traj = trajectory_pb2.Trajectory()
+
+# traj.ParseFromString(protobuf_stream)
+traj.ParseFromString(fs[16:])
+
+print len(traj.node)
+print type(traj)
+
+# traj.node.add()
+# print len(traj.node)
+
+for t in traj.node:
+    print t.timestamp, t.pose
