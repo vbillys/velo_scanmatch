73ea4fb2f90fc7762f86b29b21713e1627c6b421
Billy <billy_saputra@i2r.a-star.edu.sg>
2018-03-05 00:05:54 +0800

    now gzip works ok.

--- scripts/test_proto.py
+++ scripts/test_proto.py
@@ -2,8 +2,9 @@
 import trajectory_pb2
 import zlib
 
-# f = open('/home/agoy/Documents/denso_calib_2/denso_calib_2_0.pbstream','r');
-f = open('/home/agoy/Documents/denso_calib_2/test.pbstream','r');
+f = open('/home/agoy/Documents/denso_calib_2/denso_calib_2_0.pbstream','r');
+# f = open('/home/agoy/Documents/denso_calib_2/test.pbstream','r');
+# f = open('/home/agoy/Documents/denso_calib_2/zlib.pbstream','r');
 fs = f.read()
 s = fs[0:8];
 print list(bytearray(s))
@@ -16,19 +17,23 @@ s = fs[8:16]
 print list(bytearray(s))
 print map(hex,bytearray(s))
 
-# s = fs[16:221616]
-# print len(s)
+# s = fs[16:54829+16]
+s = fs[16:]
+print len(s)
 # # protobuf_stream = zlib.decompress(s, 15 + 32)
-# # protobuf_stream = zlib.decompress(s, zlib.MAX_WBITS | 16)
+# protobuf_stream = zlib.decompress(s, zlib.MAX_WBITS | 16)
 # protobuf_stream = zlib.decompress(fs[16:221616], zlib.MAX_WBITS | 16)
-# protobuf_stream = zlib.decompress(fs[16:221616], zlib.MAX_WBITS | 32)
+# with autoheader detection
+protobuf_stream = zlib.decompress(s, zlib.MAX_WBITS | 32)
 
-# print len(protobuf_stream)
+print len(protobuf_stream)
+# print len(fs[16:])
 
 traj = trajectory_pb2.Trajectory()
 
-# traj.ParseFromString(protobuf_stream)
-traj.ParseFromString(fs[16:])
+traj.ParseFromString(protobuf_stream)
+# traj.ParseFromString(fs[16:])
+# traj.ParseFromString(zlib.decompress(fs[16:]))
 
 print len(traj.node)
 print type(traj)
